b2d0450b67fc5c88e250adc994a70f4fe1afeab5
WojciechMitera <113037823+WojciechMitera@users.noreply.github.com>
2025-04-02 13:05:01 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    class Data
+    {
+        
+
+        public int ID{ get ; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string  PESEL { get; set; }
+
+        public Data()
+        {
+            ID = 0;
+            Name = "Szymon";
+            Surname = "Przeklasa";
+            PESEL = "12345678901";
+        }
+        public void assign(int _ID, string _Name, string _Surname, string _PESEL)
+        {
+            ID = _ID;
+            Name = _Name;
+            Surname = _Name;
+            PESEL = _PESEL;
+        }
+    }
+        public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnadd_Click(object sender, RoutedEventArgs e)
+        {
+            //var person1 = new Data();
+            //listview.Items.Add(person1);
+
+
+            var win1 = new Window1();
+
+            
+            //this.Close();
+            win1.ShowDialog();
+           
+            int id2 = win1.id2;
+            string name2 = win1.name2;
+            string surname2 = win1.surname2;
+            string pesel2 = win1.pesel2;
+            var item = new Data();
+            item.assign(id2, name2, surname2, pesel2);
+            listview.Items.Add(item);
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    
+    public partial class Window1 : Window
+         
+    {
+        public string name2 = "";
+        public string surname2 = "";
+        public string pesel2 = "";
+        public int id2 = 0;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void goback_Click(object sender, RoutedEventArgs e)
+        {
+            
+            name2 = nameAdd.Text;
+            surname2 = surnameAdd.Text;
+            pesel2 = peselAdd.Text;
+            
+            
+            //var mainwin = new MainWindow();
+            this.Close();
+            //mainwin.ShowDialog();
+        }
+    }
+}
